ab3b567896bc1202204df2e70dbfff99d9aa8ffd
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2022-06-27 22:47:41 +0200

    Add project files.

---
+++ Hangman/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Hangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a random
+            //Create a list of string
+            //Acces the string from the list
+           var random = new Random();
+           var listWords = new List<string>{"computer", "programming", "animals", " fruits", "vegetables", "cars" };
+           var index = random.Next(listWords.Count);
+
+        }
+    }
+}
